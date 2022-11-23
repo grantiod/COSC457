ad3baaa82c3ce4253ea36a6761697d85f402de8d
grantiod <giodice1212@gmail.com>
2022-11-23 10:55:04 -0500

    updated

--- sql.py
+++ sql.py
@@ -1,4 +1,4 @@
-from tkinter import Entry, Tk, END, mainloop, Text, Button
+from tkinter import Entry, Tk, END, mainloop, Button
 import MySQLdb
 
 class SQL:
@@ -7,7 +7,7 @@ class SQL:
         self.root.title('SQL')
 
     def run(self):
-        self.entry = Text(self.root, width=50, height=10)
+        self.entry = Entry(self.root, width=50)
         self.entry.grid(row=0, column=0, padx=5, pady=5)
         self.submit_btn = Button(self.root, text="Submit", command=self.submit)
         self.submit_btn.grid(row=1, column=0, padx=5, pady=5)
@@ -18,6 +18,11 @@ class SQL:
     def submit(self):
         query = self.entry.get()
         self.entry.delete(0, END)
+
+        query = query.lower()
+
+        if query[0:5] == 'select':
+            pass
 
 if __name__ == '__main__':
     s = SQL()
